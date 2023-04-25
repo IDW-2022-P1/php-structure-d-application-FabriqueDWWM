26084ff8319fe901cbf7be9d57f9ddc9e28e4764
webew <fillion.bertrand@gmail.com>
2023-04-25 11:21:10 +0200

    src+htaccess

--- index.php
+++ index.php
@@ -1,38 +1,14 @@
 <?php 
-include './config/config.php'; 
+include './src/config/config.php'; 
 require_once(CLASSES . '/Router.php');
 
 $router = new Router();
-$routes = include 'config/routes.php';
+$routes = include 'src/config/routes.php';
 $page = $router->getPage();
 
 $titre = isset($routes[$page]) ? $routes[$page] : "404";
 $pageLink = CSS . "/" . $page . ".css";
+
+
+include TEMPLATE . "/base.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= TITLE ?></title>
-    <link rel="stylesheet" href="<?= CSS ?>/style.css">
-    <?php if (file_exists($pageLink)) : ?>
-        <link rel="stylesheet" href="<?= $pageLink ?>">
-    <?php endif ?>
-    <link rel="stylesheet" href="<?= CSS ?>/responsive.css">
-    <script defer src="<?= JS ?>/main.js"></script>
-</head>
-
-<body>
-    <?php
-    include TEMPLATE_PARTS . '/_header.php';
-
-    $router->includeTemplate();
-    
-    include TEMPLATE_PARTS . '/_footer.php'; ?>
-
-</body>
-
-</html>
---
+++ src/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('TEMPLATE', './src/template');
+define('TEMPLATE_PARTS', './src/template/template_parts');
+define('PAGES', './src/template/pages/');
+define('FONCTIONS', './src/fonctions');
+define('CLASSES', './src/classes');
+
+define('IMAGES', './public/assets/img');
+define('CSS', './public/assets/css');
+define('JS', './public/assets/js');
+
+define('TITLE', 'Pierre, Paul, Jacques');
